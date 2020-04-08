9c5653a892d57e1a366a1547fba6dcc4bacf2591
Lautenai Junior <lautenai@gmail.com>
2020-04-08 16:53:15 -0300

    first commit

---
+++ application/views/home/two.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Laravel: A Framework For Web Artisans</title>
+	<meta name="viewport" content="width=device-width">
+	{{ HTML::style('laravel/css/style.css') }}
+	{{ HTML::script('js/turbolinks.js')}}
+</head>
+<body>
+	<div class="wrapper">
+		<header>
+			<h1>Laravel</h1>
+			<h2>A Framework For Web Artisans</h2>
+
+			<p class="intro-text" style="margin-top: 45px;">
+			</p>
+		</header>
+		<div role="main" class="main">
+			<div class="home">
+				<h2>Learn the terrain.</h2>
+
+				<h1><a href="one">One</a></h1>
+				<h1><a href="two">Two</a></h1>
+				<h1><a href="three">Three</a></h1>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
